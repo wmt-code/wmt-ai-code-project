b6cb71a21c7a8a2a9250a259053863b73cefc880
Wmt <2631527748@qq.com>
2025-08-17 03:15:45 +0800

    项目初始化

--- src/main/java/com/wmt/wmtaicode/WmtAiCodeApplication.java
+++ src/main/java/com/wmt/wmtaicode/WmtAiCodeApplication.java
@@ -2,12 +2,14 @@ package com.wmt.wmtaicode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class WmtAiCodeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WmtAiCodeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WmtAiCodeApplication.class, args);
+	}
 
 }
--- src/main/java/com/wmt/wmtaicode/controller/HealthController.java
+++ src/main/java/com/wmt/wmtaicode/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.wmt.wmtaicode.controller;
 
+import com.wmt.wmtaicode.common.BaseResponse;
+import com.wmt.wmtaicode.common.ResultUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
 	@GetMapping("/check")
-	public String health() {
-		return "OK";
+	public BaseResponse<String> health() {
+		return ResultUtils.success("ok");
 	}
 }
