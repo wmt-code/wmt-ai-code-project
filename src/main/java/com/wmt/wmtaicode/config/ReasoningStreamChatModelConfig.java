ce8a34c4f5ef6f1777f3cae0f541dd117154a2e4
Wmt <2631527748@qq.com>
2025-09-02 18:31:10 +0800

    切换为deepseek-reasoner模型

--- src/main/java/com/wmt/wmtaicode/config/ReasoningStreamChatModelConfig.java
+++ src/main/java/com/wmt/wmtaicode/config/ReasoningStreamChatModelConfig.java
@@ -22,11 +22,11 @@ public class ReasoningStreamChatModelConfig {
 	@Bean
 	public StreamingChatModel reasoningStreamChatModel() {
 		// 测试环境使用deepseek-chat模型
-		final String modelName = "deepseek-chat";
-		final int maxTokens = 8192;
+		// final String modelName = "deepseek-chat";
+		// final int maxTokens = 8192;
 		// 生产环境使用deepseek-reasoner模型
-		// final String modelName = "deepseek-reasoner";
-		// final int maxTokens = 32768;
+		final String modelName = "deepseek-reasoner";
+		final int maxTokens = 32768;
 		return OpenAiStreamingChatModel.builder()
 				.apiKey(apiKey)
 				.baseUrl(baseUrl)
