b0f2a1353ac75c5725dae359af9eba27cfdc10bc
Wmt <2631527748@qq.com>
2025-09-10 16:48:27 +0800

    删除底部footer

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -4,14 +4,11 @@
     <a-layout-content class="flex-1 py-6 md:py-4">
       <router-view />
     </a-layout-content>
-    <GlobalFooter />
   </a-layout>
-
 </template>
 
 <script setup lang="ts">
 import GlobalHeader from '@/components/GlobalHeader.vue'
-import GlobalFooter from '@/components/GlobalFooter.vue'
 </script>
 
 <style scoped></style>
